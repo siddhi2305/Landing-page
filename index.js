bde203521f5ee385bb0da85f3d98446337c3b215
Siddhi kadam <119724423+siddhi2305@users.noreply.github.com>
2023-09-28 20:08:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementById("toggle-btn")
+const navmenu = document.getElementById("nav-menu")
+toggleBtn.addEventListener("click", ()=>{
+    navmenu.classList.toggle("active")
+})
+
